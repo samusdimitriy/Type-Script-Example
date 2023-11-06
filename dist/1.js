84a10eb047b91520ad086f404a492a3348e61768
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-11-06 10:46:42 +0100

    readonly

--- dist/1.js
+++
@@ -1,8 +0,0 @@
-function combine(a, b) {
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-export {};
-//# sourceMappingURL=1.js.map
